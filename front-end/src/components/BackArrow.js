97450d8d6787c34f44171ff40cb006204d326fdd
Zain Bokhari <45834770+zbokhari@users.noreply.github.com>
2020-07-11 15:36:53 -0400

    Delete BackArrow.js

--- front-end/src/components/BackArrow.js
+++
@@ -1,5 +0,0 @@
-BackArrow = () => (
-    <div onClick={previousImage} style={{ fontSize: '2em', marginRight: '12px' }}>
-      <i className="fa fa-angle-left fa-2x" aria-hidden="true"></i>
-    </div>
-  )
