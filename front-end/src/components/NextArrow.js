ac941a4e09063edfa7ac1d7b22d03d4828e62e9d
Zain Bokhari <45834770+zbokhari@users.noreply.github.com>
2020-07-11 15:36:42 -0400

    Delete NextArrow.js

--- front-end/src/components/NextArrow.js
+++
@@ -1,5 +0,0 @@
-NextArrow = () => (
-    <div onClick={nextImage} style={{ fontSize: '2em', marginLeft: '12px' }}>
-      <i className="fa fa-angle-right fa-2x" aria-hidden="true"></i>
-    </div>
-  )
